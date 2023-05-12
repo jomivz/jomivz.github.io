91fedd4bf8991d165ccdda725a6e18bb2f459a6b
jomivz <jomivz@orange.fr>
2023-05-12 15:52:53 +0200

    up

--- playbook/pen_cracked_accounts.sh
+++ playbook/pen_cracked_accounts.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 # usage: ./pen_cracked_accounts.sh secretdumps.out
-args="$@"
-secretsdump="${args[0]}"
-#potfile="${args[1]}"
-potfile="/home/$LOGNAME/.local/share/hashcat/hashcat.potfile"
+secretsdump="$1"
+potfile="$2"
+#potfile="/home/$LOGNAME/.local/share/hashcat/hashcat.potfile"
 
 if [[ ! -r $secretsdump ]]; then
 	echo $secretsdump.": INVALID SECRETDUMPS FILE."
-	echo $potfile
 	exit;
 fi
 
 echo "NO CRACKED ACCOUNT." > cracked_accounts.txt
 
 if [[ -e $potfile && -r $potfile ]]; then
-	echo "" > cracked_account.txt
-
+	echo "" > cracked_accounts.txt
 	cat $potfile | while read line; do
 		cracked_hash=`echo $line | cut -f1 -d:`
-		grep $cracked_hash $secretsdump >> cracked_accounts.txt
+		grep $cracked_hash $secretsdump | cut -f1 -d: >> cracked_accounts.txt
 	done
 fi
 
